54c01b75a5190fac4759df7a09c89dedd00c19cc
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-03 14:31:00 +0000

    Initial Ad Custom Post Type

--- inc/wpas-ad.php
+++ inc/wpas-ad.php
@@ -2,5 +2,18 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class WPAdServer_Ad {
-
+  function wpas_create_post_type() {
+    register_post_type("wpas-ad",
+      array(
+       "labels" => array(
+         "name" => __("Ads"),
+         "singular_name" => __("Ad")
+       ),
+       "show_in_admin_bar" => true,
+       "menu_position" => 20,
+       "taxonomies" => array("wpas-ad-zone")
+     )
+   );
+  }
 }
+add_action("init", "wpas_create_post_type");
