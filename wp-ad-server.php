8e6300acffc578b375faede5c06b0ddc122cf36a
Jeff "Huey" Huelsbeck <huey@carpetelam.com>
2019-05-03 13:33:32 +0000

    Initialize plugin

---
+++ wp-ad-server.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: WP Ad Server
+ * Plugin URI: https://github.com/thisishuey/wordcamp-2019-plugin-workshop
+ * Description: Serve ads for company
+ * Version 1.0
+ * Author: Jake Sutherland & Jeff "Huey" Huelsbeck
+ * License: GPLv3
+ */
