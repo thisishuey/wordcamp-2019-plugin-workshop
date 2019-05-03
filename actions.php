585c036a8b2583ece353f7c35d4001fd8d2a75d7
Jake Sutherland <zoic@zoic.me>
2019-05-03 10:35:48 -0400

    Fixed ad post type registration and display in admin

--- actions.php
+++ actions.php
@@ -1,5 +1,9 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+// Register the 'Ad' post type.
+add_action( 'init', array( 'WPAdServer_Ad', 'wpas_create_post_type' ) );
+
 // Register the 'Ad Zone' taxonomy.
 add_action( 'init', array( 'WPAdServer_Ad_Zone', 'register' ) );
+
--- inc/wpas-ad.php
+++ inc/wpas-ad.php
@@ -2,13 +2,14 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class WPAdServer_Ad {
-  function wpas_create_post_type() {
+  public static function wpas_create_post_type() {
     register_post_type("wpas-ad",
       array(
        "labels" => array(
          "name" => __("Ads"),
          "singular_name" => __("Ad")
-       ),
+	   ),
+	   "show_ui" => true,
        "show_in_admin_bar" => true,
        "menu_position" => 20,
        "taxonomies" => array("wpas-ad-zone")
@@ -16,4 +17,3 @@ class WPAdServer_Ad {
    );
   }
 }
-add_action("init", "wpas_create_post_type");
